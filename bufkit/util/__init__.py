e675cb8d3ae4e6250332d7f4799c1cdff2bd7285
Carter Humphreys <carter.humphreys@lake-effect.dev>
2024-01-26 06:08:34 -0700

    Moved code into src and updated dependencies in setup.py

--- bufkit/util/__init__.py
+++
@@ -1,1 +0,0 @@
-from .units import units
--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
    name="bufkit-api",
    version="1.0.0",
    description="Provides a Python interface to BUFKIT BUFR files.",
    url="https://github.com/HumphreysCarter/bufkit-api",
    author="Carter Humphreys",
-   packages=setuptools.find_packages(include=["bufkit*"])
+   packages=find_packages(),
+   package_dir={'bufkit': 'src/bufkit'},
+   install_requires=[
+      'metpy',
+   ],
+   python_requires='>=3.9',
 )
